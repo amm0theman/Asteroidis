5ffaea6150d4a11a0cf53bf6a1e51c6558e5d335
amm0theman <ammonriley.97@gmail.com>
2018-09-30 23:01:06 -0600

    Added bullets.py and gameState.py

---
+++ collisionManager.py
@@ -0,0 +1,20 @@
+"""#This function will handle the collision of objects"""
+
+from gameState import GameState
+
+
+class CollisionManager:
+    def __init__(self, game_state):
+        self.game_state = GameState(game_state)
+
+    #gotta check each intersection with each other
+    #compare each intersection
+    #
+    def if_intersect(self):
+        for i in self.game_state.asteroid:
+
+
+
+
+
+
