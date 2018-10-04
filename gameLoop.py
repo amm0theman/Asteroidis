5bec559ca55ad1846e3efae12a50bcfcbce0bef6
amm0theman <ammonriley.97@gmail.com>
2018-10-03 20:17:12 -0600

    Added collision manager, and added as loop for chekcing intersections.

---
+++ gameLoop.py
@@ -0,0 +1,38 @@
+import pygame
+from gameState import GameState
+
+
+class GameLoop:
+    def __init__(self):
+        '# Initialize game window and settings etc.'
+        self.screen = pygame.display.set_mode(
+            (1000, 1000))
+        pygame.display.set_caption("Asteroids")
+        self.game_active = True
+        self.game_state = GameState(None, None, None, None)
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            '# When x button pushed quit game'
+            if event.type == pygame.QUIT:
+                self.game_active = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.game_active = True
+
+        '# Sets the screen to next render'
+        pygame.display.flip()
+
+    def update_game(self):
+        pass
+
+    def render_game(self):
+        pass
+
+    """Main game loop"""
+    def run_game(self):
+        pygame.init()
+
+        while self.game_active:
+            self.handle_events()
+            self.update_game()
+            self.render_game()
--- mainFile.py
+++ mainFile.py
@@ -1,40 +1,8 @@
 import pygame
-from gameState import GameState
-from movementManager import MovementManager
+from gameLoop import GameLoop
 
 
-class Game:
-    """Contains game loop and game objects, and manages them"""
-    def __init__(self):
-        self.screen = pygame.display.set_mode(
-            (1000, 1000))
-        pygame.display.set_caption("Asteroids")
-        self.game_active = True
-        self.game_state = GameState(None, None, None, None)
-        self.MovementManager = MovementManager(None, None, None)
 
-    def handle_events(self):
-        for event in pygame.event.get():
-            '# When x button pushed quit game'
-            if event.type == pygame.QUIT:
-                self.game_active = False
+class Main:
 
-        '# Sets the screen to next render'
-        pygame.display.flip()
-
-    """Update Models for next frame"""
-    def update_game(self):
-        pass
-
-    """Draw next frame and game objects"""
-    def render_game(self):
-        pass
-
-    """Main game loop"""
-    def run_game(self):
-        pygame.init()
-
-        while self.game_active:
-            self.handle_events()
-            self.update_game()
-            self.render_game()
+GameLoop.run_game()
